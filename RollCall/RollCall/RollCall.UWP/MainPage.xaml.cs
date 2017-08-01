6c457345d92a5614563ccc0e3b00ca58d1f47275
Brian Foote <bfoote@rushlakeconsulting.net>
2017-08-01 14:32:54 -0500

    first commit

---
+++ RollCall/RollCall/RollCall.UWP/MainPage.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace RollCall.UWP
+{
+    public sealed partial class MainPage
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+            LoadApplication(new RollCall.App());
+        }
+    }
+}
